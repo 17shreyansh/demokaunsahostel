57f4e5f59b56de13b9949c59c99b7201dc51ad78
Shreyansh <kake0shreyansh@gmail.com>
2026-06-15 01:57:03 +0530

    update

--- project/backend/server.js
+++ project/backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(helmet());
 app.use(compression());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://kaunsahostel.com', 'https://www.kaunsahostel.com'],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
